6c053789c7d05158ae2424d68899f75278485250
Alexey Kerpel <alexeykerpel15@gmail.com>
2023-06-14 12:33:50 +0300

    Initial Commit

---
+++ talker/talkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  talkerApp.swift
+//  talker
+//
+//  Created by Alexey Kerpel on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct talkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
